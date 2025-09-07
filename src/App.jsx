34cec3e84c3d11a234c9846cdc6674c3f40fa7ed
Adarsh07-Parihar <pariharmanas85@gmail.comm>
2025-09-07 12:41:17 +0530

    pixabay_clone

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Images from './components/images'
+import NavBar from './components/NavBar'
+
+const App = () => {
+  return (
+    <div>
+      <NavBar/>
+      <Images/>
+    </div>
+  )
+}
+
+export default App
